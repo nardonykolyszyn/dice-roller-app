90b910614f847158a50e8d117a6ce7e496ed38cc
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-07-12 04:32:18 -0500

    add roller functionality

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // NOTE: @IBOutlet allows to reference a UI element.
+    @IBOutlet weak var diceHandlerOne: UIImageView!
+    @IBOutlet weak var diceHandlerTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollHandler(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceHandlerOne.image = diceArray[Int.random(in: 0...5)]
+        diceHandlerTwo.image = diceArray[Int.random(in: 0...5)]
+    }
 }
-
